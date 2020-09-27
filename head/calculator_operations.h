d67ae287ceb5dab73e2268edac9494b6571703e2
99002579 <71488045+99002579@users.noreply.github.com>
2020-09-27 22:42:48 +0530

    Update and rename inc.h to calculator_operations.h

--- head/calculator_operations.h
+++ head/calculator_operations.h
@@ -1,3 +1,15 @@
+** 
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
 /**
 *  adds the operand1 and operand2 and returns the result
 * @param[in] operand1 
@@ -29,3 +41,7 @@ int multiply(int operand1, int operand2);
 * @param[in] operand2 
 * @return integer value of the operand1 / operand2
 * @note returns 0 for divide by 0 error
+*/
+int divide(int operand1, int operand2);
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
